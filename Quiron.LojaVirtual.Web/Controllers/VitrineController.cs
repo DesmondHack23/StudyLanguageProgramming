cd1c62e1aa9c828837d307acc44ef1f9bb58bb07
DesmondHack23 <alex_mix_estoni@hotmail.com>
2016-05-11 12:25:22 -0300

    Paginas view

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -1,25 +1,25 @@
-﻿using System;
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Quiron.LojaVirtual.Dominio.Repositorio;
 
 namespace Quiron.LojaVirtual.Web.Controllers
 {
     public class VitrineController : Controller
     {
         private ProdutosRepositorio _repositorio;
-        public int ProdutosPorPagina = 8;
+        public int ProdutosPorPagina = 3;
+
+        // GET: Vitrine
         public ActionResult ListaProdutos(int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
             var produtos = _repositorio.Produtos
                 .OrderBy(p => p.Descricao)
-                .Skip((pagina - 1)*ProdutosPorPagina)
-                .Take(ProdutosPorPagina);
-
-
+            .Skip((pagina - 1) * ProdutosPorPagina)
+            .Take(ProdutosPorPagina);
 
             return View(produtos);
         }
